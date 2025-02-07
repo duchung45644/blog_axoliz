2ed8372190a28142aff9cfa8153608c70627f9db
Liz <duchung45644@gmail.com>
2025-02-07 15:28:29 +0700

    Refactor Docker setup and configuration; utilize .env for environment variables and improve caching in Dockerfile

--- backend/app/config/base.py
+++ backend/app/config/base.py
@@ -1,15 +1,16 @@
+import os
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Axoliz"
-    PROJECT_VERSION: str = "1.0.0"
-    DATABASE_URL: str = ""  # Set this in .env
+    PROJECT_NAME: str
+    PROJECT_VERSION: str
+    DATABASE_URL: str = ""
     ALLOWED_ORIGINS: list = ["http://vps.axoliz.com", "http://vps.axoliz.com:5432"]
 
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 300  # Token hết hạn sau 300 phút
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
